34dbc0debbcc0b51220d7faaa12ca5ca5f65a646
tuhindewan <tuhinsshadow@gmail.com>
2017-03-21 07:41:45 -0700

    design index.php

--- index.php
+++ index.php
@@ -6,25 +6,74 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Login Register System PHP OOP</title>
     <link href="inc/bootstrap.min.css" rel="stylesheet">
+    <script type="text/javascript" src="inc/jquery-3.1.1.min.js"></script>
     <script type="text/javascript" src="inc/bootstrap.min.js"></script>
   </head>
   <body>
     
       <div class="container">
-        <nav class="navbar navbar-default">
-          <div class="container-fluid">
-            <div class="navbar-header">
-              <a class="navbar-brand" href="index.php">Login Registration System Using OOP PHP</a>
-            </div>
-              <ul class="nav navbar-nav navbar-right">
-                <li><a href="profile.php"></a>Profile</li>
-                <li><a href="register.php"></a>Register</li>
-                <li><a href="login.php"></a>Login</li>
-                <li><a href=""></a>Logout</li>
-              </ul>
-          </div>
-        </nav>
+          <nav class="navbar navbar-default">
+              <div class="container-fluid">
+                <div class="navbar-header">
+                  <a class="navbar-brand" href="index.php">Login Registration System</a>
+                </div>
+                  <ul class="nav navbar-nav navbar-right">
+                        <li><a href="profile.php">Profile</a></li>
+                        <li><a href="register.php">Registration</a></li>
+                        <li><a href="login.php">Login</a></li>
+                        <li><a href="#">Logout</a></li>
+                      </ul>
+                </div>
+          </nav>
+                  <div class="panel panel-default">
+                          <div class="panel-heading">
+                         <h2>User List <span class="pull-right"><strong>Welcome! </strong>Tuhin</span></h2>
+                          </div>
+                            <div class="panel-body">
+                              <table class="table table-striped">
+                              <tr>
+                              <th width="20%">Serial</th>
+                              <th width="20%">Name</th>
+                              <th width="20%">Username</th>
+                              <th width="20%">Email Address</th>
+                              <th width="20%">Action</th>
+                              </tr>
+                              <tr>
+                                <td>01</td>
+                                <td>tuhin</td>
+                                <td>tuhin</td>
+                                <td>tuhin@gmail.com</td>
+                                <td>
+                                  <a class="btn btn-primary" href="profile.php?id=1">view</a>
+                                </td>
+                              </tr>
+                              <tr>
+                                <td>01</td>
+                                <td>tuhin</td>
+                                <td>tuhin</td>
+                                <td>tuhin@gmail.com</td>
+                                <td>
+                                  <a class="btn btn-primary" href="profile.php?id=1">view</a>
+                                </td>
+                              </tr>
+                              <tr>
+                                <td>01</td>
+                                <td>tuhin</td>
+                                <td>tuhin</td>
+                                <td>tuhin@gmail.com</td>
+                                <td>
+                                  <a class="btn btn-primary" href="profile.php?id=1">view</a>
+                                </td>
+                              </tr>
+                              </table>
+                            </div>
+                   </div>
+
+                <div class="well">
+                  <h3>Github:https://github.com/tuhindewan
+                  <span class="pull-right">Follow Me:https://web.facebook.com/saiduzzaman.tuhin.1</span>
+                  </h3>
+                </div>
       </div>
-   
   </body>
 </html>
